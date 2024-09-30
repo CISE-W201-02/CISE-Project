751696c7ead71dd23e2c1fa1b52b0da535b78e35
AidanDarlington <aidanlukedarlington@gmail.com>
2024-09-30 21:58:55 +1300

    Commit 1.0.1
    - Added MongoDB database connection and functionality
    - Added functionality based on the lab 3 book creation
    - Change directories to streamline development

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import ShowBookList from "@/components/ShowBookList";
+import React from "react";
+
+export default function Home() {
+  return (
+    <main>
+      <ShowBookList />
+    </main>
+  );
+}
